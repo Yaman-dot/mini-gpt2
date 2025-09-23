6466cb5ecde3a7ca7aa7c771842948ef69e8080d
Yaman Abulaban <yamanabulaban@gmail.com>
2025-09-23 17:51:56 +0300

    Build the Block layers

--- train.py
+++ train.py
@@ -5,7 +5,17 @@ from torch.nn import functional as F
 
 
 
-
+class Block(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.ln1 = nn.LayerNorm(config.n_embd)
+        self.attn = CausalSelfAttention(config)
+        self.ln2 = nn.LayerNorm(config.n_embd)
+        self.mlp = MLP(config)
+    def forward(self, x):
+        x = x + self.attn(self.ln1(x))
+        x = x + self.mlp(self.ln2(x))
+        return x
 @dataclass
 class Config:
     block_size: int = 256
